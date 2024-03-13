0f2da04d93833c234410bf62df2994ff4ffca379
Narendra Kumar Nishad <nknishad678@gamil.com>
2024-03-13 15:52:12 +0530

    Vite_React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai  from "./chai"
+function App() {
+  const username = "Narendar"
+ 
+  return(
+    <>
+    <Chai />
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+    </ >
+  )
+
+  
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>Hello stacklab.in from h2 tag</h2>
+    )
+}
+export default Chai
