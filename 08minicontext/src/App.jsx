c15bf859aaf51a44357d2dcf779d0c3d33b2abb6
Narendra Kumar Nishad <nknishad678@gamil.com>
2024-03-21 15:28:11 +0530

    Context_api

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './Componet/Login'
+import Profile from './Componet/Profile'
+
+function App() {
+
+
+  return (
+    <>
+    <UserContextProvider>
+    <h1>Stacklab aur Context Api</h1>
+    <Login/>
+    <Profile/>
+    </UserContextProvider>
+    </>
+  )
+}
+
+export default App
